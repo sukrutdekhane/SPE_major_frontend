67c9034181c82619996eca1e09a28ab56bfe6cae
sukrutdekhane <dekhanesd7@gmail.com>
2023-04-10 19:08:59 +0530

    Created signup page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/user/Navbar'
 import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
 
 export default function App() {
   
     return (
     <>
     <Navbar />
-    <Signin />
+    <Signup />
     </>
     );
 
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -5,18 +5,21 @@ import Forminput from '../form/Forminput'
 import Submit from '../form/Submit'
 
 
-export default function Signin() {
+export default function Signup() {
  
 return <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
     <Container>
         <form className="bg-secondary rounded p-6 w-72 space-y-6">
-           <Title> Sign in</Title>
+           <Title> Sign up</Title>
+          <Forminput label="First Name" placeholder="Sukrut" name="name" />
+          <Forminput label="Last Name" placeholder="Dekhane" name="name" />
+          <Forminput label="Phone Number" placeholder="0123456789" name="phone number" />
           <Forminput label="Email" placeholder="john@example.com" name="email" />
           <Forminput label="Password" placeholder="********" name="password" />
-          <Submit value="Sign in" />
+          <Submit value="Sign up" />
           <div className="flex justify-between">
             <a className="text-dark-subtle hover:text-white transition" href="#" >Forget Password</a>
-            <a className="text-dark-subtle hover:text-white transition" href="#" >Sign in</a> 
+            <a className="text-dark-subtle hover:text-white transition" href="#" >Sign up</a> 
           </div>
         </form>
     </Container>
