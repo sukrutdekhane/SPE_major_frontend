c16d0a3e4da45692e5523f07fd0869a6f2651ff1
sukrutdekhane <dekhanesd7@gmail.com>
2023-04-16 19:11:02 +0530

    OTP verification

--- src/components/auth/PhoneVerification.jsx
+++ src/components/auth/PhoneVerification.jsx
@@ -1,7 +1,81 @@
 import React from 'react'
+import Container from '../container'
+import Title from '../form/Title';
+import Submit from '../form/Submit';
+import { useState,useEffect,useRef} from "react";
 
+const OTP_LENGTH=6;
+
+let currentOTPIndex;
 export default function PhoneVerification() {
+  const [otp,setOtp]=useState(new Array(OTP_LENGTH).fill(''))
+  const[activeOtpIndex,setActiveOtpIndex]=useState(0);
+  
+  const inputRef=useRef()
+
+  const focusNextInputField=(index)=>{
+    setActiveOtpIndex(index+1)
+  }
+
+  const focusPrevInputField=(index)=>{
+    let nextIndex;
+        const diff=index-1
+        nextIndex= diff!==0?diff:0
+    setActiveOtpIndex(nextIndex)
+  }
+
+  const handleOtpChange = ({target}) => { 
+      
+      const{value}=target;
+      const newOtp=[...otp]
+      newOtp[currentOTPIndex]=value.substring(value.length-1, value.length);
+     
+      if(!value) focusPrevInputField(currentOTPIndex)
+      else
+      focusNextInputField(currentOTPIndex)
+      
+      
+      setOtp([...newOtp]);
+
+};
+
+const handleKeyDown = ({ key }, index) => {
+  currentOTPIndex = index;
+  if (key === "Backspace") {
+      focusPrevInputField(currentOTPIndex);
+   }
+};
+
+  useEffect(()=>{
+    inputRef.current?.focus()
+
+  },[activeOtpIndex]);
   return (
-    <div>PhoneVerification</div>
-  )
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+    <Container>
+        <form className="bg-secondary rounded p-6 space-y-6">
+           <div>
+           <Title> Please enter the OTP to verify your account</Title>
+           <p className="text-center text-dark-subtle">OTP has been sent to your email</p>
+           </div>
+           <div className="flex justify-center items-center space-x-4">
+          {otp.map((_, index) => {
+            return(
+               <input
+               ref={activeOtpIndex===index? inputRef : null}
+               key={index}
+                type="number" 
+                value={otp[index] || ""}
+                onChange={handleOtpChange}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                className= "w-12 h-12 border-2 border-dark-subtle first-letter: focus:border-white rounded bg-transparent outline-none text-center text-white font-semibold text-xl"/>
+            );
+          })}
+          </div>
+          <Submit value="Send Link" />
+         
+        </form>
+    </Container>
+</div>
+  );
 }
